07d0ae12b82e329a95776937561eea70cfc766a5
arcturus <arcturus@ardeenelinfierno.com>
2012-09-21 23:50:04 +0100

    First version of the score function

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var request = require('request');
 var redis = require('redis'),
     redisClient = redis.createClient();
 
+var FIRST_TIME = 10;
+
 var END_POINT = 'https://drivetoimprove.co.uk/api/m/TelemetryInfoByType/%SERVICE_ID%?from=%START%&to=%END%&type=1,3,4,25,13,51,52,94';
 var events = {};
 var getData = function getData(user, pass, service,
@@ -29,11 +31,11 @@ var getData = function getData(user, pass, service,
       return;
     }
 
-    onSuccess(JSON.parse(body));
+    onSuccess(service, JSON.parse(body));
   });
 };
 
-var parseData = function parseData(points) {
+var parseData = function parseData(service, points) {
   var re1 = new RegExp("[0-9]+\b(.+)", "ig");
   var re2 = new RegExp(".+ \\[(.+)\\]", "ig");    
   points.GetTelemetryInfoByTypeResult.forEach(function(point) {    
@@ -67,22 +69,57 @@ var parseData = function parseData(points) {
       var bigRoad = streetSplit[1];
       console.log(bigRoad);
       events[bigRoad] = events[bigRoad] || [];
-      events[bigRoad] = cEvent;
+      events[bigRoad].push(cEvent);
     }
     events[streetName] = events[streetName] || [];
-    events[streetName] = cEvent;
+    events[streetName].push(cEvent);
   });
 
   for (var st in events) {
-    var score = getScore(events[st]);
-    console.log(st + " SCORE " + score);
+    getScore(service, st, events[st], function(scoreObj) { 
+      console.log('Score for street ' + scoreObj.street + ' : ' + scoreObj.score);
+    });
   }
   redisClient.quit();
-}
+};
 
-var getScore = function(events) {
-  //TODO
-  return 0;
+var getScore = function(service, street, events, callback) {
+  var scoresByEvents = {
+    '1': 1, // Gps tick
+    '3': 1, // Journey start
+    '4': 1, // Journey ends
+    '25': 1, // Heartbeat
+    '13': -1, // Overspeed
+    '51': -1, // Harsh acceleration
+    '52': -1, // Harsh breaking
+    '94': -10 // Crash
+  };
+  var key = keyCounter(service, street);
+  redisClient.incrby(key, events.length, 
+    function (err, value) {
+      var score = value;
+      if (value == events.length) {
+        //First time
+        score += FIRST_TIME;
+      }
+
+      for(var i = 0; i < events.length; i++) {
+        score += scoresByEvents[events[i].type];
+      }
+
+      callback({
+        street: street,
+        score: score
+      });
+  }); 
+};
+
+var _key = function (service, street) {
+  return street + ':' + service;
+};
+
+var keyCounter = function(service, street) {
+  return _key(service, street) + ':' + 'count';
 }
 
 if (!process.argv || process.argv.length !=7) {
